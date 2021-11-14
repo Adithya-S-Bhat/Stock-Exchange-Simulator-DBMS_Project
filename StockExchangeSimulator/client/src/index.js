30db36638a3df0615b036312e4bd1edfd3b52467
Adithya M S <adithyashivaram@gmail.com>
2021-11-14 15:41:55 +0530

    dark mode

--- StockExchangeSimulator/client/src/index.js
+++ StockExchangeSimulator/client/src/index.js
@@ -1,12 +1,23 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const darktheme=createMuiTheme({
+  palette:{
+      type:"dark",
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={darktheme}>
+    <React.StrictMode>
+      <CssBaseline/>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
